ea3d2b832e408b89ad15015d238c4ca430b7f81d
Tayse Rosa <tayse_rosa@yahoo.com.br>
2024-11-05 14:19:01 -0300

    Start the project HCS

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Image 
+        source={require('./src/assets/logobranco.png')} 
+        resizeMode='contain'
+        style={styles.logo}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00547A',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo:{
+    width:300,
+  }
 });
